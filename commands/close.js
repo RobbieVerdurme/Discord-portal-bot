b13d1d720e5eb8e6a4ba048ef321fdb81af89aa5
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-24 22:33:43 +0200

    [Add] close command

---
+++ commands/close.js
@@ -0,0 +1,21 @@
+const { channels } = require('../config.json');
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+module.exports = {
+    name: 'close',
+    description: 'close portal',
+    async execute(message, args) {
+        if (message.channel.name.includes(channels)) {
+            message.channel.send('You can\'t delete the this channel')
+        } else {
+            const millisecondsSleep = 10000
+
+            message.channel.send('That\'s enough for today lets watch some interdimensional cable');
+            await sleep(millisecondsSleep)
+            message.channel.delete().catch(console.log)
+        }
+    }
+};
