7d5ec178ef18d12ec9d9f4698382a693c3e8ef3c
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-24 22:33:53 +0200

    [Add] portal command

---
+++ commands/portal.js
@@ -0,0 +1,32 @@
+var GphApiClient = require('giphy-js-sdk-core')
+const { giphyToken } = require('../config.json');
+
+giphy = GphApiClient(giphyToken)
+
+module.exports = {
+    name: 'portal',
+    description: 'Portal do an other dimension!',
+    execute(message, args) {
+        const randomNumberDimension = Math.round(Math.random() * 1000)
+        message.guild.channels.create(`dimension-${randomNumberDimension}`).then(channel => {
+            // set topic channel
+            channel.setTopic(`Welcome to dimension ${randomNumberDimension}`)
+
+            // search gif portal
+            giphy.search('gifs', { 'q': 'portal rick and morty' }).then((response) => {
+                const totalResponses = response.data.length
+                const responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses
+                const responseGif = response.data[responseIndex]
+
+                // send message in new channel
+                channel.send(`Welcome to dimension ${randomNumberDimension}`, {
+                    files: [responseGif.images.fixed_height.url]
+                });
+
+            }).catch(() => {
+                // Error
+                channel.send('I\'m to drunk for this shit')
+            })
+        })
+    },
+};
