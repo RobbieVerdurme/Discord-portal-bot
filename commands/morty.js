62cab5e6df91e4f7deead2f9c6f4e5113d8d1930
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-25 18:02:21 +0200

    [Add] morty command that secures the channel

---
+++ commands/morty.js
@@ -0,0 +1,17 @@
+const { channels } = require('../config.json');
+
+module.exports = {
+    name: 'morty',
+    description: 'This adds the channel so it can no longer be deleted by the bot',
+    execute(message, args) {
+        const { cache } = message.guild.roles
+        const role = cache.find(role => role.name.toLowerCase() === 'rick')
+
+        if (role.permissions.has('ADMINISTRATOR')) {
+            channels.push(message.channel.name)
+            message.channel.send('Channel secure')
+        } else {
+            message.channel.send('Your sutch a dick')
+        }
+    },
+};
