5992c557d3413662f9d2c241be30072d7ffda775
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-25 17:42:06 +0200

    [Update] close command add role restriction

--- commands/close.js
+++ commands/close.js
@@ -8,14 +8,24 @@ module.exports = {
     name: 'close',
     description: 'close portal',
     async execute(message, args) {
+        const role = message.member.roles.cache.find(role => role.name === message.channel.name)
+
+        // check channel
         if (message.channel.name.includes(channels)) {
             message.channel.send('You can\'t delete the this channel')
-        } else {
+
+            // check member has role
+        } else if (message.member.roles.cache.has(role.id)) {
             const millisecondsSleep = 10000
 
             message.channel.send('That\'s enough for today lets watch some interdimensional cable');
             await sleep(millisecondsSleep)
+
+            role.delete()
             message.channel.delete().catch(console.log)
+
+        } else {
+            message.channel.send('You don\'t have the right role to delete this channel')
         }
     }
-};
+}
